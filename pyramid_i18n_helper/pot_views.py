6b979133f6b8af0e08871eec6bf744ae1e339118
me <s.h.mahdavi@chmail.ir>
2017-04-20 16:44:12 +0430

    start creating lang form

--- pyramid_i18n_helper/pot_views.py
+++ pyramid_i18n_helper/pot_views.py
@@ -9,33 +9,44 @@ import os
 
 @view_defaults(route_name='pot', renderer='templates/pot.jinja2', permission='admin')
 class PotView():
-    def __init__(self, context, request:Request):
+    def __init__(self, context, request: Request):
         self.request = request
         self.context = context
         _ = request.translate
 
         self.helper = request.registry['i18n_helper']
 
+        # MSG FORM
         class MessageID(colander.SequenceSchema):
             msgid = colander.SchemaNode(colander.String())
 
-
         class MainSchema(colander.Schema):
             msgid = MessageID(title="msgid", widget=widget.SequenceWidget(orderable=True))
-
 
         def validator(node, appstruct):
             # TODO:
             return True
 
-
         schema = MainSchema(validator=validator)
         schema = schema.bind(request=request)
-        self.form = deform.Form(schema, use_ajax=False, action=request.route_url('pot'))
-        self.form.buttons.append(deform.Button(name='submit', title=_('i18n_pot_submit')))
+        self.msg_form = deform.Form(schema, use_ajax=False, action=request.route_url('pot'))
+        self.msg_form.buttons.append(deform.Button(name='submit', title=_('i18n_pot_submit')))
 
-        self.pot = polib.pofile(os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
+        self.pot = polib.pofile(
+            os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
 
+
+        # LANG FORM
+        class NewLang(colander.Schema):
+            new_lang = colander.SchemaNode(colander.String(), title="New Lang")
+
+        def validator(node, appstruct):
+            return True
+
+        schema = NewLang(validator=validator)
+        schema = schema.bind(request=self.request)
+        self.lang_form = deform.Form(schema, use_ajax=False, action=self.request.route_url('pot'))
+        self.lang_form.buttons.append(deform.Button(name='submit', title='submit'))
 
     @view_config(request_method="GET")
     def get_view(self):
@@ -48,13 +59,18 @@ class PotView():
         for entry in self.pot:
             entries.append(entry.msgid)
 
-        form_data = {'msgid': entries}
+        msg_form_data = {'msgid': entries}
 
-        return {'form': self.form, 'form_data': form_data}
+        langs = os.listdir(os.path.join(self.helper.package_dir, 'locale'))
+        print(langs)
 
+        return {'msg_form': self.msg_form,
+                'msg_form_data': msg_form_data,
+                'lang_form': self.lang_form
+                }
 
-    @view_config(request_method='POST')
-    def post_view(self):
+    @view_config(request_method='POST', request_param='msgid')
+    def msg_view(self):
 
         request = self.request
         context = self.context
@@ -64,8 +80,7 @@ class PotView():
         controls = request.POST.items()
 
         try:
-            appstruct = self.form.validate(controls)
-            print(appstruct)
+            appstruct = self.msg_form.validate(controls)
 
         except:
             appstruct = None
@@ -77,8 +92,20 @@ class PotView():
                 entry = polib.POEntry(msgid=msgid)
                 self.pot.append(entry)
 
-            self.pot.save(os.path.join(self.helper.package_dir, 'locale','{0}.pot'.format(self.helper.package_name)))
+            self.pot.save(os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
 
-        self.pot = polib.pofile(os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
+        self.pot = polib.pofile(
+            os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
+
+        return self.get_view()
+
+    @view_config(request_method='POST', request_param='new_lang')
+    def lang_view(self):
+
+        lang = self.request.POST.get('new_lang', '').strip()
+        os.mkdir(os.path.join(self.helper.package_dir, 'locale', lang))
+        os.mkdir(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES'))
+        self.pot.save(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.po'.format(self.helper.package_name)))
+        self.pot.save_as_mofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.mo'.format(self.helper.package_name)))
 
         return self.get_view()
