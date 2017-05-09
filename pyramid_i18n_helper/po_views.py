d19df878635e20e7592f467af96fcb47cd6ba294
me <s.h.mahdavi@chmail.ir>
2017-05-09 09:19:58 +0430

    reload po

--- pyramid_i18n_helper/po_views.py
+++ pyramid_i18n_helper/po_views.py
@@ -7,39 +7,30 @@ import polib
 import os
 
 
-
 @view_defaults(route_name='po', renderer='templates/po.jinja2', permission='admin')
 class PoView():
-    def __init__(self, context, request:Request):
-
+    def __init__(self, context, request: Request):
         self.request = request
         self.context = context
         _ = request.translate
-
         self.helper = request.registry['i18n_helper']
-
-
         self.lang = request.matchdict['lang']
-        lang = self.lang
+        self.po = polib.pofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES',
+                                            '{0}.po'.format(self.helper.package_name)))
 
-        self.po = polib.pofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.po'.format(self.helper.package_name)))
-        po = self.po
-        self.po_entries = {entry.msgid: entry.msgstr for entry in po}
-        po_entries = self.po_entries
-
+    def form_creator(self):
+        po_entries = {entry.msgid: entry.msgstr for entry in self.po}
 
         class PoEntry(colander.Schema):
-
             def after_bind(self, schema, kwargs):
                 for entry in po_entries:
                     self[entry] = colander.SchemaNode(
                         colander.String(),
                         title=entry,
-                        missing = '',
-                        default = po_entries.get(entry),
+                        missing='',
+                        default=po_entries.get(entry),
                         # description=po_entries.get(entry)
                     )
-
 
         class MainSchema(colander.Schema):
             msgid = PoEntry()
@@ -49,11 +40,11 @@ class PoView():
             return True
 
         schema = MainSchema(validator=validator)
-        schema = schema.bind(request=request)
-        self.form = deform.Form(schema, use_ajax=False, action=request.route_url('po', lang=lang))
+        schema = schema.bind(request=self.request)
+        self.form = deform.Form(schema, use_ajax=False, action=self.request.route_url('po', lang=self.lang))
         self.form.buttons.append(deform.Button(name='submit', title='submit'))
         self.form.buttons.append(deform.Button(name='reload', title='reload'))
-
+        return self.form
 
     @view_config(request_method="GET")
     def get_view(self):
@@ -61,6 +52,8 @@ class PoView():
         request = self.request
         context = self.context
         _ = request.translate
+
+        self.form_creator()
 
         return {"form": self.form}
 
@@ -71,8 +64,6 @@ class PoView():
         context = self.context
         _ = request.translate
         lang = self.lang
-        po = self.po
-
 
         controls = request.POST.items()
 
@@ -89,12 +80,12 @@ class PoView():
             for entry in self.po:
                 entry.msgstr = appstruct['msgid'].get(entry.msgid, '')
 
-            po.save(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.po'.format(self.helper.package_name)))
-            po.save_as_mofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.mo'.format(self.helper.package_name)))
+            self.po.save(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES',
+                                      '{0}.po'.format(self.helper.package_name)))
+            self.po.save_as_mofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES',
+                                                '{0}.mo'.format(self.helper.package_name)))
 
         return self.get_view()
-
-
 
     @view_config(request_param='reload')
     def reload_view(self):
@@ -107,14 +98,16 @@ class PoView():
         lang = request.matchdict['lang']
 
         pot = polib.pofile(os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
-        po = polib.pofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.po'.format(self.helper.package_name)))
-        po_entries = {entry.msgid: entry.msgstr for entry in po}
+        self.po = polib.pofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES',
+                                            '{0}.po'.format(self.helper.package_name)))
+        po_entries = {entry.msgid: entry.msgstr for entry in self.po}
 
         for entry in pot:
             if not entry.msgid in po_entries:
-                po.append(entry)
+                self.po.append(entry)
 
-        po.save(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.po'.format(self.helper.package_name)))
-        po.save_as_mofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES', '{0}.mo'.format(self.helper.package_name)))
+        self.po.save(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES',
+                                  '{0}.po'.format(self.helper.package_name)))
+        self.po.save_as_mofile(os.path.join(self.helper.package_dir, 'locale', lang, 'LC_MESSAGES',
+                                            '{0}.mo'.format(self.helper.package_name)))
         return self.get_view()
-
