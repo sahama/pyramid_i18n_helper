4492c831e3060143239b042caf127d5afba8256e
sahama <sayyid.hamid.mahdavi@gmail.com>
2017-05-13 14:55:17 +0000

    debug

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
 
 
 setup(name='pyramid_i18n_helper',
-      version='0.2.6.4',
+      version='0.2.6.5',
       description='Small tool to help in i18n and l10n',
       long_description=README,
       classifiers=[
