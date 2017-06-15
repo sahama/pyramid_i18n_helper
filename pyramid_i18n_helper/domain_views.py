7ec4272f8c46b768bb8080f0d4e8ad4074f4ce4b
me <s.h.mahdavi@chmail.ir>
2017-06-15 17:24:39 +0430

    domain view

--- pyramid_i18n_helper/domain_views.py
+++ pyramid_i18n_helper/domain_views.py
@@ -1,4 +1,5 @@
 from pyramid.view import view_config, view_defaults
+from pyramid.httpexceptions import HTTPFound
 import colander
 import deform
 from deform import widget
@@ -15,6 +16,37 @@ class PoView():
         self.context = context
         _ = request.translate
 
+        self.helper = request.registry['i18n_helper']
+
+
+        # Domain FORM
+        pot_dir = os.path.join(self.helper.package_dir, 'locale')
+        domains_choices = [(pot.rsplit('.',maxsplit = 1)[0],pot.rsplit('.',maxsplit = 1)[0]) for pot in os.listdir(pot_dir) if pot.endswith('.pot')]
+
+        class SelectDomain(colander.Schema):
+            select_domain = colander.SchemaNode(colander.String(),
+                                              widget=deform.widget.SelectWidget(values=domains_choices),
+                                              title=_("domains"))
+
+        def validator(node, appstruct):
+            return True
+
+
+
+        schema = SelectDomain(validator=validator)
+        schema = schema.bind(request=self.request)
+        self.select_domain_form = deform.Form(schema,
+                                            use_ajax=False,
+                                            action=self.request.route_url('i18n_helper.domain'))
+        self.select_domain_form.buttons.append(deform.Button(name='submit', title=_('i18n_select_domain_submit')))
+
     @view_config(request_method="GET")
     def get_view(self):
-        return {}
+
+        return {'form': self.select_domain_form}
+
+    @view_config(request_method="POST")
+    def post_view(self):
+        domain = self.request.POST.get('select_domain', '').strip()
+
+        return HTTPFound(location=self.request.route_url('i18n_helper.pot', domain=domain))
