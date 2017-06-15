8d0a319331454d1d8b0ea286081d4437e6ef86b0
me <s.h.mahdavi@chmail.ir>
2017-06-15 11:19:02 +0430

    handle duplication error

--- pyramid_i18n_helper/pot_views.py
+++ pyramid_i18n_helper/pot_views.py
@@ -20,21 +20,6 @@ class PotView():
 
         self.helper = request.registry['i18n_helper']
 
-        # MSG FORM
-        class MessageID(colander.SequenceSchema):
-            msgid = colander.SchemaNode(colander.String())
-
-        class MainSchema(colander.Schema):
-            msgid = MessageID(title="msgid", widget=widget.SequenceWidget(orderable=True))
-
-        def validator(node, appstruct):
-            # TODO: some validation
-            return True
-
-        schema = MainSchema(validator=validator)
-        schema = schema.bind(request=request)
-        self.msg_form = deform.Form(schema, use_ajax=False, action=request.route_url('pot'))
-        self.msg_form.buttons.append(deform.Button(name='submit', title=_('i18n_pot_submit')))
 
         self.pot = polib.pofile(
             os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
@@ -81,11 +66,32 @@ class PotView():
         self.select_lang_form = deform.Form(schema, use_ajax=False, action=self.request.route_url('pot'))
         self.select_lang_form.buttons.append(deform.Button(name='submit', title=_('i18n_select_lang_submit')))
 
+        # MSG FORM
+    def msg_form_creator(self):
+        request = self.request
+        _ = request.translate
+
+        class MessageID(colander.SequenceSchema):
+            msgid = colander.SchemaNode(colander.String())
+
+        class MainSchema(colander.Schema):
+            msgid = MessageID(title="msgid", widget=widget.SequenceWidget(orderable=True))
+
+        def validator(node, appstruct):
+            # TODO: some validation
+            return True
+
+        schema = MainSchema(validator=validator)
+        schema = schema.bind(request=self.request)
+        self.msg_form = deform.Form(schema, use_ajax=False, action=self.request.route_url('pot'))
+        self.msg_form.buttons.append(deform.Button(name='submit', title=_('i18n_pot_submit')))
+        return self.msg_form
+
     @view_config(request_method="GET")
     def get_view(self):
         request = self.request
         context = self.context
-
+        self.msg_form = self.msg_form_creator()
         _ = request.translate
 
         entries = []
@@ -94,12 +100,17 @@ class PotView():
 
         msg_form_data = {'msgid': entries}
 
+        print(msg_form_data)
 
-        return {'msg_form': self.msg_form,
+
+        return_dict = {'msg_form': self.msg_form,
                 'msg_form_data': msg_form_data,
                 'new_lang_form': self.new_lang_form,
                 'select_lang_form': self.select_lang_form,
                 }
+
+        print(return_dict)
+        return return_dict
 
     @view_config(request_method='POST', request_param='msgid')
     def msg_view(self):
@@ -114,11 +125,6 @@ class PotView():
         try:
             appstruct = self.msg_form.validate(controls)
 
-        except:
-            appstruct = None
-            self.request.flash_message.add(message_type='danger', body='i18n_pot_msg_data_not_valid')
-
-        if appstruct:
             self.pot = polib.POFile()
 
             for msgid in set(appstruct['msgid']):
@@ -127,6 +133,9 @@ class PotView():
 
             self.pot.save(os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
             self.request.flash_message.add(message_type='success', body='i18n_pot_msg_data_process_success')
+
+        except:
+            self.request.flash_message.add(message_type='danger', body='i18n_pot_msg_data_not_valid')
 
         self.pot = polib.pofile(
             os.path.join(self.helper.package_dir, 'locale', '{0}.pot'.format(self.helper.package_name)))
